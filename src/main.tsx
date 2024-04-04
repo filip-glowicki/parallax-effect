04e14964862ea6836eaf1830ebfda42a33dabe45
Filip Głowicki <filip.glowicki@outlook.com>
2024-04-04 12:18:03 +0200

    Fix import order

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,10 @@ import ReactDOM from "react-dom/client";
 import { Section } from "./components/section";
 import { ParallaxProvider } from "react-scroll-parallax";
 import { HeroSection } from "./components/hero-section";
+import { SmallScreenAlert } from "./components/small-screen-alert";
 
 import "./styles/css-reset.css";
 import "./styles/main.css";
-import { SmallScreenAlert } from "./components/small-screen-alert";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
