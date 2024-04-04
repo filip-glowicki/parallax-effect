6e6615eb18ebce86d35de9e0d0f4d7161fe24f9d
Filip Głowicki <filip.glowicki@outlook.com>
2024-04-04 12:14:45 +0200

    Refactor code

--- src/components/small-screen-alert.tsx
+++ src/components/small-screen-alert.tsx
@@ -1,8 +1,8 @@
 import comingSoon from "../assets/coming-soon.svg";
 
 export const SmallScreenAlert = () => (
-  <div className="mobile-container">
+  <section className="mobile-container">
     <img src={comingSoon} alt="We are working on mobile version" />
     <p>Version for smaller screens is coming soon</p>
-  </div>
+  </section>
 );
